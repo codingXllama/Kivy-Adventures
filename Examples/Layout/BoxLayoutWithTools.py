756133a57f954a0c4b2b6857b53d31c86f7e4b47
codingxllama <osamaalchalabi2@gmail.com>
2019-08-30 19:26:00 -0600

    Created BoxLayout example with padding and more

---
+++ Examples/Layout/BoxLayoutWithTools.py
@@ -0,0 +1,21 @@
+from kivy.uix.boxlayout import  BoxLayout
+from kivy.app import App
+from kivy.lang import   Builder
+from kivy.base import runTouchApp
+
+
+runTouchApp(Builder.load_string("""
+
+BoxLayout:
+    orientation: 'vertical'
+    spacing: 10
+    padding: 100
+    Button:
+        text:'Hello'
+    Button:
+        text: 'Goodbye'
+
+
+"""))
+
+
