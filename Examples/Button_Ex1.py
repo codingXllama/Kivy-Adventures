fdced620f668905d8e9fb24348185d78f7d658ae
codingxllama <osamaalchalabi2@gmail.com>
2019-09-06 19:56:53 -0600

    Created another btn

--- Examples/Button_Ex1.py
+++ Examples/Button_Ex1.py
@@ -6,12 +6,20 @@ from kivy.base import runTouchApp
 runTouchApp(Builder.load_string("""
  
 Label:
+#Creating 
     Button:
         text:'btn1'
         font_size:32
         color:0.8,0.9,0,1
         pos:50,100
+        size:100,200
         
+    Button:
+        text:'btn2'
+        font_size:40
+        color:0.1,0.9,0.5,1
+        size:200,100
+        pos:200,100
                                 
                                 
                                 
